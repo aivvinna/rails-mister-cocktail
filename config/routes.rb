acb7b90c29a2dff4c0835fc3a9a16fe12271d49d
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-09 17:35:18 +0900

    backend done except for 2 rake errors, some frontend done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :doses do
-  #   resources :cocktails
-  #   resources :ingredients
-  # end
+  root to: "cocktails#index"
+  resources :cocktails, only: [ :index, :new, :create, :show, :destroy] do
+    resources :doses, only: [ :new, :create, :show]
+  end
+  resources :doses, only: [ :destroy ]
 end
