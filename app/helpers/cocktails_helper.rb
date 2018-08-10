b6e65a9ae04a913f2ef51d7602cd14fcf51dc7b3
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-10 19:49:20 +0900

    fixed bug with no image upload

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def cocktail_photo_or_default(cocktail)
+    if cocktail.photo.present?
+      cl_image_path cocktail.photo, height: 300, width: 400, crop: :fill
+    else
+      image_path 'cocktaillogo.jpg'
+    end
+  end
 end
