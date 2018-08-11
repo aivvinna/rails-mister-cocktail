c53aebdc730cd93e0620e553ad7b19325c285789
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-11 18:00:46 +0900

    changed some colors on navbar and text

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,9 +3,9 @@ function initUpdateNavbarOnScroll() {
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-wagon-white');
+        navbar.classList.add('navbar-wagon-pink');
       } else {
-        navbar.classList.remove('navbar-wagon-white');
+        navbar.classList.remove('navbar-wagon-pink');
       }
     });
   }
